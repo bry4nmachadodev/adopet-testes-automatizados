cbdce59f2a531c87af11ce392185934046b5376f
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-31 14:11:37 -0300

    implementando oque será necessário para usar mock.

--- src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
+++ src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -1,7 +1,49 @@
 package br.com.alura.adopet.api.validacoes;
 
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.hibernate.validator.constraints.ModCheck;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ValidacaoPetDisponivelTest {
+
+    //anotação que indica que metodos dessa classe vao ser usados com mocks
+    @InjectMocks
+    private ValidacaoPetDisponivel validacao;
+
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
+    @Test
+    @DisplayName("Deveria Permitir A Solicitação De Adoção Do Pet")
+    void cenario1(){
+        //arrange
+        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(
+                7l, 2l, "Motivo qualquer"
+        );
+
+        BDDMockito.given(petRepository.getReferenceById(id)).willReturn(pet);
+
+
+        //assert + act
+        Assertions.assertDoesNotThrow(() -> validacao.validar(dto));
+    }
 
 }
