51f431136620431efdb54082accc6dd54189e28f
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-31 15:10:00 -0300

    limpando implementações não usadas.

--- src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamentoTest.java
+++ src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamentoTest.java
@@ -2,11 +2,8 @@ package br.com.alura.adopet.api.validacoes;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.exception.ValidacaoException;
-import br.com.alura.adopet.api.model.Adocao;
-import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
-import br.com.alura.adopet.api.repository.PetRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,8 +12,6 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class ValidacaoPetComAdocaoEmAndamentoTest {
