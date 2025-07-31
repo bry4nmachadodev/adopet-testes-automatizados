4d5e8413a6439a27d0f4b8dbecdad186984c32c4
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-31 15:04:25 -0300

    criação 1 cenário teste ValidacaoPetComAdocaoEmAndamentoTest (working).

--- src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamentoTest.java
+++ src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamentoTest.java
@@ -1,7 +1,50 @@
 package br.com.alura.adopet.api.validacoes;
 
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ValidacaoPetComAdocaoEmAndamentoTest {
 
+    @Mock
+    private PetRepository petRepository;
+    @Mock
+    private AdocaoRepository adocaoRepository;
+
+    @InjectMocks
+    private ValidacaoPetComAdocaoEmAndamento validacao;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
+    @Test
+    @DisplayName("Deveria retornar uma Validação Exception devido a adoção do pet já estar com andamento")
+    void cenario01(){
+        //arrange
+        BDDMockito.given(dto.idPet()).willReturn(1L);
+        BDDMockito.given(adocaoRepository
+                        .existsByPetIdAndStatus(dto.idPet(), StatusAdocao.AGUARDANDO_AVALIACAO))
+                .willReturn(true);
+
+        //assert + act
+        Assertions.assertThrows(ValidacaoException.class, () -> validacao.validar(dto));
+    }
 }
