c02591a9002e6e09382290904faee67af231bbd3
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-01 13:43:59 -0300

    criação da classe teste para O Adoção Service.

---
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.adopet.api.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdocaoServiceTest {
+
+    @Test
+    void deveriaSalvarAdocaoAoSolicitar(){
+
+    }
+
+}
