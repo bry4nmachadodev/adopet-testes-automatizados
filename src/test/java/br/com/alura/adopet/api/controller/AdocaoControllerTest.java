68772f138311828bc7aaf36ddc1a56d9ad2ca125
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-04 17:02:39 -0300

    criação da classe test do controller de adoção.

---
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,16 @@
+package br.com.alura.adopet.api.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AdocaoControllerTest {
+
+    @Test
+    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros(){
+
+    }
+}
