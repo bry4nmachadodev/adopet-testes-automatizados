333eb23de10d9339d599711ca785a4bf134125ae
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-04 17:27:09 -0300

    criando teste na AdocaoControllerTest.

--- src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -4,23 +4,36 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class AdocaoControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros(){
+    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() throws Exception {
         //ARRANGE
         String json = "{}";
 
+        //ACT -> onde é feita a requisição para o controller (usando o MOCK MVC)
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andReturn().getResponse();
 
-        //ACT
-
-
-
-        //ASSERT
-        Assertions.assertEquals(400, );
-
+        //ASSERT -> pega o response e verifica seu status
+        Assertions.assertEquals(400, response.getStatus());
     }
 }
