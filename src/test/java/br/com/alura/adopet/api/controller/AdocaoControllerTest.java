17dd8607487876755526ea5277fcba51e863b3fc
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-04 17:13:23 -0300

    removendo anotações.

--- src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -1,16 +1,26 @@
 package br.com.alura.adopet.api.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
 class AdocaoControllerTest {
 
     @Test
     void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros(){
+        //ARRANGE
+        String json = "{}";
+
+
+        //ACT
+
+
+
+        //ASSERT
+        Assertions.assertEquals(400, );
 
     }
 }
