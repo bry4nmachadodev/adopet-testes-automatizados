b18ea9a4b6233e3e5834ca1be6b304bf3f712d0c
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-05 16:52:20 -0300

    criação dos 2 cénarios. (without work)

--- src/test/java/br/com/alura/adopet/api/service/AbrigoServiceTest.java
+++ src/test/java/br/com/alura/adopet/api/service/AbrigoServiceTest.java
@@ -1,8 +1,12 @@
 package br.com.alura.adopet.api.service;
 
+import br.com.alura.adopet.api.repository.AbrigoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,9 +14,34 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class AbrigoServiceTest {
 
+    @InjectMocks
+    private AbrigoService service;
+
+    @Mock
+    private AbrigoRepository abrigoRepository;
+
+    @Mock
+    private PetRepository petRepository;
+
     @Test
-    @DisplayName("")
+    @DisplayName("deve dar ValidacaoException devido a DADOS JÁ CADASTRADOS")
     void cenario01(){
+    //ARRANGE
+
+    //ACT
+
+    //ASSERT
+
+    }
+
+    @Test
+    @DisplayName("deve SALVAR no banco")
+    void cenario02(){
+        //ARRANGE
+
+        //ACT
+
+        //ASSERT
 
     }
 }
