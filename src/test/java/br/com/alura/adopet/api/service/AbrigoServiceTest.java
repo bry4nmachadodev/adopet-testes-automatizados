73352ee737e14e96bc3a14322c33def002fb8db0
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-05 17:02:03 -0300

    desenvolvimento do 1 cénario AbrigoServiceTest. (working)

--- src/test/java/br/com/alura/adopet/api/service/AbrigoServiceTest.java
+++ src/test/java/br/com/alura/adopet/api/service/AbrigoServiceTest.java
@@ -1,10 +1,13 @@
 package br.com.alura.adopet.api.service;
 
+import br.com.alura.adopet.api.dto.CadastroAbrigoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
 import br.com.alura.adopet.api.repository.AbrigoRepository;
 import br.com.alura.adopet.api.repository.PetRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -23,15 +26,17 @@ class AbrigoServiceTest {
     @Mock
     private PetRepository petRepository;
 
+    private CadastroAbrigoDto cadastroDto;
+
     @Test
     @DisplayName("deve dar ValidacaoException devido a DADOS JÁ CADASTRADOS")
     void cenario01(){
-    //ARRANGE
+        //ARRANGE
+        this.cadastroDto = new CadastroAbrigoDto("Casinha Feliz", "1134567890", "casinhafeliz@teste.com");
+        BDDMockito.given(abrigoRepository.existsByNomeOrTelefoneOrEmail(cadastroDto.nome(), cadastroDto.telefone(), cadastroDto.email())).willReturn(true);
 
-    //ACT
-
-    //ASSERT
-
+        //ASSERT + ACT
+        assertThrows(ValidacaoException.class, () -> service.cadastrar(cadastroDto));
     }
 
     @Test
