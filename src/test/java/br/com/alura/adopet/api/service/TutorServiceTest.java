51767227680ca4bbea528a8e313aa92205898c9d
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-06 17:25:09 -0300

    criação da classe TutorServiceTest.

---
+++ src/test/java/br/com/alura/adopet/api/service/TutorServiceTest.java
@@ -0,0 +1,18 @@
+package br.com.alura.adopet.api.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TutorServiceTest {
+
+    @Test
+    @DisplayName("")
+    void cenario01(){
+
+    }
+}
