34c3cbc8f24970fb2295e658953ab6d2adf2f021
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-06 18:49:08 -0300

    criação do 06 cénario AbrigoControllerTest. (working)

--- src/test/java/br/com/alura/adopet/api/controller/AbrigoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AbrigoControllerTest.java
@@ -1,16 +1,26 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.dto.CadastroAbrigoDto;
+import br.com.alura.adopet.api.dto.CadastroPetDto;
+import br.com.alura.adopet.api.dto.PetDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.TipoPet;
 import br.com.alura.adopet.api.service.AbrigoService;
 import br.com.alura.adopet.api.service.PetService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -28,6 +38,16 @@ class AbrigoControllerTest {
 
     @MockBean
     private PetService petService;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private Abrigo abrigo;
+
+    private CadastroPetDto cadastroPetDto;
+
+    private PetDto petDto;
 
     @Test
     @DisplayName("deveria DEVOLVER código 200 para o GET -> LISTA DE ABRIGOS (DEVIDO AO JSON INVÁLIDO")
@@ -110,9 +130,34 @@ class AbrigoControllerTest {
     }
 
     @Test
-    @DisplayName("")
-    void cenario01GetListarPets(){
+    @DisplayName("Deveria retornar os PETS do ABRIGO")
+    void cenario01GetListarPets() throws Exception {
+        //ARRANGE
+        CadastroAbrigoDto abrigoFake = new CadastroAbrigoDto("Abrigo", "11999999999", "teste@abrigo.com");
+        Abrigo abrigo1 = new Abrigo(abrigoFake);
 
+        CadastroPetDto petDto1 = new CadastroPetDto(TipoPet.CACHORRO, "Rex", "Vira-lata", 3, "Marrom", 12.5f);
+        Pet pet1 = new Pet(petDto1, abrigo1);
+        PetDto petDtoResponse1 = new PetDto(pet1);
+
+        CadastroPetDto petDto2 = new CadastroPetDto(TipoPet.GATO, "Mimi", "Siamês", 2, "Branco", 5.0f);
+        Pet pet2 = new Pet(petDto2, abrigo1);
+        PetDto petDtoResponse2 = new PetDto(pet2);
+
+        List<PetDto> petsDoAbrigo = List.of(petDtoResponse1, petDtoResponse2);
+
+        BDDMockito.given(abrigoService.listarPetsDoAbrigo("Abrigo")).willReturn(petsDoAbrigo);
+
+        //ACT
+        var response = mvc.perform(get("/abrigos/Abrigo/pets"))
+                .andReturn()
+                .getResponse();
+
+        //ASSERT
+        Assertions.assertEquals(200, response.getStatus());
+        String jsonResponse = response.getContentAsString();
+        Assertions.assertTrue(jsonResponse.contains("Rex"));
+        Assertions.assertTrue(jsonResponse.contains("Mimi"));
     }
 
 }
