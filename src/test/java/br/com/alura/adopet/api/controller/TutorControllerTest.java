5bdce0811bb9e971c5b24002cae63342d91a99f3
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-07 14:24:51 -0300

    criação da classe TutorControllerTest. (not working)

---
+++ src/test/java/br/com/alura/adopet/api/controller/TutorControllerTest.java
@@ -0,0 +1,35 @@
+package br.com.alura.adopet.api.controller;
+
+import br.com.alura.adopet.api.dto.AtualizacaoTutorDto;
+import br.com.alura.adopet.api.dto.CadastroTutorDto;
+import br.com.alura.adopet.api.service.TutorService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TutorControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private TutorService service;
+
+    private CadastroTutorDto tutorDto;
+
+    private AtualizacaoTutorDto atualizacaoTutorDto;
+
+    @Test
+    @DisplayName("")
+    void cenario01(){
+
+    }
+}
