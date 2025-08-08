8237a6ee68783cc5b1baf491e7bac5ff4cf8b94c
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-08 13:18:54 -0300

    limpando PetControllerTest.

--- src/test/java/br/com/alura/adopet/api/controller/PetControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/PetControllerTest.java
@@ -1,20 +1,15 @@
 package br.com.alura.adopet.api.controller;
 
 import br.com.alura.adopet.api.dto.PetDto;
-import br.com.alura.adopet.api.model.TipoPet;
 import br.com.alura.adopet.api.service.PetService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
